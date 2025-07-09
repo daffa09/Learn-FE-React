4e1c090a822edf95a16ce0c64ea2efd5c92f97df
daffa09 <daffa.fathan9@gmail.com>
2025-07-09 10:54:33 +0700

    day 3 checkpoint 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import PostDetail from "./pages/PostDetail";
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import './App.css'
+import { Button } from './components/ui/button'
+import Home from './pages/Home'
+import About from './pages/About'
+import Posts from './pages/Posts'
+import PostDetail from './pages/PostDetail'
+
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/post/:id" element={<PostDetail />} />
-    </Routes>
-  );
+      <BrowserRouter>
+        <div className="w-full flex gap-4 p-4 justify-center border-b mb-8">
+          <Button asChild variant="outline">
+            <Link to="/">Home</Link>
+          </Button>
+          <Button asChild variant="outline">
+            <Link to="/about">About</Link>
+          </Button>
+          <Button asChild variant="outline">
+            <Link to="/posts">Posts</Link>
+          </Button>
+        </div>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/about' element={<About/>} />
+          <Route path='/posts' element={<Posts/>} >
+            <Route path=':postId' element={<PostDetail/>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+  )
 }
